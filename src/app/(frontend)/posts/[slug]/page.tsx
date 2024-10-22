66db59df0753db5418cd217c7aecd5b121f6718d
Josh Chang <joshchang04@gmail.com>
2024-10-22 01:59:28 -0400

    more optimization

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -102,6 +102,7 @@ export default async function Post({ params: paramsPromise }: Args) {
             <MediaFigure
               className="w-full h-auto"
               media={post.cover}
+              priority
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 60vw"
             />
           </div>
