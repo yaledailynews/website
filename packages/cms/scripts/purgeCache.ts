68f1c35f42e00df1ef433c55b923620b9afebba1
Josh Chang <joshchang04@gmail.com>
2024-11-15 05:11:43 -0500

    add purge cache script to build

---
+++ packages/cms/scripts/purgeCache.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod'
+
+const env = z
+  .object({
+    CLOUDFLARE_ZONE_ID: z.string().min(1),
+    CLOUDFLARE_API_KEY: z.string().min(1),
+    SITE_HOST: z.string().min(1),
+  })
+  .parse(process.env)
+
+console.log('Purging Cloudflare cache')
+
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${env.CLOUDFLARE_API_KEY}`,
+  },
+  body: JSON.stringify({ purge_everything: true }),
+}
+const res = await fetch(
+  `https://api.cloudflare.com/client/v4/zones/${env.CLOUDFLARE_ZONE_ID}/purge_cache`,
+  options,
+)
+const data = await res.json()
+console.log(data)
+if (data.success) {
+  console.log('Successfully purged cache')
+} else {
+  throw new Error('Failed to purge Cloudflare cache')
+}
